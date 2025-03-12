85380d9285ca6def78dc757d9523b6312fd8c512
w-bonelli <wesbonelli@gmail.com>
2025-03-12 09:38:39 -0400

    skip backwards tracking examples for now

--- autotest/test_mf6_examples.py
+++ autotest/test_mf6_examples.py
@@ -6,10 +6,17 @@ from modflowapi import run_simulation
 examples = models.get_examples()
 pytestmark = pytest.mark.mf6
 dll = "libmf6"
+skip = [
+    # https://github.com/MODFLOW-ORG/modflowapi/issues/53
+    "ex-prt-mp7-p02",
+    "ex-prt-mp7-p04",
+]
 
 
 @pytest.mark.parametrize("example_name", examples.keys())
 def test_example(function_tmpdir, example_name):
+    if example_name in skip:
+        pytest.skip(f"Skipping example {example_name}")
     for model_name in examples[example_name]:
         model_relpath = model_name.rpartition(example_name + "/")[-1]
         model_workspace = function_tmpdir / model_relpath
